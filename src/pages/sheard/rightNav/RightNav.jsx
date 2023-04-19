69f173d3d724b02d09c77ad0c12739da208dea33
emonhasan007 <emon45088@gmail.com>
2023-04-19 07:57:02 +0600

    rightside card content added

--- src/pages/sheard/rightNav/RightNav.jsx
+++ src/pages/sheard/rightNav/RightNav.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
+import { Button, ListGroup} from "react-bootstrap";
+import { FaGoogle ,FaGithub ,FaFacebook,FaInstagram,FaTwitter} from "react-icons/fa";
+import QZone from "../qzone/QZone";
+import bg from '../../../assets/bg.png'
 
 const RightNav = () => {
-    return (
-        <div>
-            <h5>right</h5>
-        </div>
-    );
+  return (
+    <div>
+      <h4>LogIn</h4>
+      <Button variant="outline-primary"> <FaGoogle/> Login with Google</Button>
+      <Button variant="outline-secondary"> <FaGithub/> Login with Github</Button>
+
+    <div>
+    <ListGroup>
+      <ListGroup.Item><FaFacebook/>Facebook</ListGroup.Item>
+      <ListGroup.Item><FaInstagram/>Instagram</ListGroup.Item>
+      <ListGroup.Item><FaTwitter/>Twitter</ListGroup.Item>
+    </ListGroup>
+    </div>
+    <QZone></QZone>
+    <div>
+    <img src={bg} alt="" />
+    </div>
+    </div>
+
+  );
 };
 
 export default RightNav;
